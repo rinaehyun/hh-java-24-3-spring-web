b1594a3827bb563faa419aef6b1036468550da54
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-18 10:30:37 +0200

    Initial commit

---
+++ src/main/java/org/example/hhjava243springweb/HhJava243SpringWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.hhjava243springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava243SpringWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava243SpringWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hhjava243springweb/HhJava243SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hhjava243springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava243SpringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
