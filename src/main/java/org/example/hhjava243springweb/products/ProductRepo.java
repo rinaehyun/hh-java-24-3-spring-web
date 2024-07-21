203775a64e7798598d3fb4eaa183b92867b010cd
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-21 21:54:58 +0200

    Add an annotation for Repo

--- src/main/java/org/example/hhjava243springweb/products/ProductRepo.java
+++ src/main/java/org/example/hhjava243springweb/products/ProductRepo.java
@@ -1,8 +1,9 @@
 package org.example.hhjava243springweb.products;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ProductRepo extends MongoRepository<Product, String> {
     // List<Product> products = new ArrayList<>(); -> will be stored in db
     // addtional methods regarding the db and class will be modified here.
