fe07e0b5ae574d5d811465b5789e658061af2728
Rinae Hyun <rnhyun.dream@gmail.com>
2024-07-22 09:35:58 +0200

    Use lombok annotation for constructor in controller

--- src/main/java/org/example/hhjava243springweb/products/ProductController.java
+++ src/main/java/org/example/hhjava243springweb/products/ProductController.java
@@ -1,19 +1,21 @@
 package org.example.hhjava243springweb.products;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
 
     // Dependency Injection
     private final ProductRepo productRepo;
 
-    public ProductController(ProductRepo productRepo) {
-        this.productRepo = productRepo;
-    }
+    //public ProductController(ProductRepo productRepo) {
+    //    this.productRepo = productRepo;
+    //}
 
     @GetMapping
     public List<Product> getAllProducts() {
