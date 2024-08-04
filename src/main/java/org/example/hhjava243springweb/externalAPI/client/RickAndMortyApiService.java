d57805f8282c8c6ecea0b5c90caf4fec2e101ed0
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-04 12:51:31 +0200

    Use @Value to inject values from application.properties - url

--- src/main/java/org/example/hhjava243springweb/externalAPI/client/RickAndMortyApiService.java
+++ src/main/java/org/example/hhjava243springweb/externalAPI/client/RickAndMortyApiService.java
@@ -1,5 +1,6 @@
 package org.example.hhjava243springweb.externalAPI.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -8,10 +9,8 @@ public class RickAndMortyApiService {
 
     private final RestClient restClient;
 
-    public RickAndMortyApiService() {
-        restClient = RestClient.builder()
-                .baseUrl("https://rickandmortyapi.com/api")
-                .build();
+    public RickAndMortyApiService(@Value("${app.rickandmorty.api.url}") String baseUrl) {
+        restClient = RestClient.create(baseUrl);
     }
 
     public ApiResponse fetchApiResponse() {
